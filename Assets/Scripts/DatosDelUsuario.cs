94998749b244475b38c6baa152ae76011f80c686
Joaquin Schopflocher <joacoschop@gmail.com>
2026-04-17 09:01:37 -0300

    Initial commit

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombre = "Joaco";
+    public int edad = 15;
+    public float altura = 1.90f;
+    public bool donante = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es "+ nombre + ", tengo " + edad + ", mido " + altura + " metros y es " + donante + " que soy donante de órganos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    public string variableString = "hola Jero";
+    public bool variableBool = true;
+    public int variableInt = 12;
+    public float variableFloat = 12.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(variableBool);
+        Debug.Log(variableFloat);
+        Debug.Log(variableString);
+        Debug.Log(variableInt);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
